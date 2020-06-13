14052e22e2a4cdb114cd142011f5024de8d824a8
RHayabusa87 <lcuff@our.ecu.edu.au>
2020-06-13 19:44:26 +0800

    Add Week4 scripts to portfolio

---
+++ Portfolio/Week4/IPAddressesOnly.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bash IPInfo.sh | sed -n '/IP Address/p'
+
+
+
+#Two_IP_Addresses="$(ifconfig)"
+
+#echo "$et_info" | sed '/inet' / s/inet/IP Address:/
+#All_IP_Addresses=$(echo "$Two_IP_Addresses" | sed -n '/IP Address' / {
+#'s/\n\t\tIP Address/
+#p
+#}')
+
+#echo -e "The addresses of this computer are as follows:\n$All_IP_Addresses"
